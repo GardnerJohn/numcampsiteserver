6456ee0dfbabdbf5ce46647932926fc87e4a1523
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-08-25 14:46:03 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "406228257592507",
+    clientSecret: "8a0815e58a4b1d4490647475771367a1",
+  },
 };
